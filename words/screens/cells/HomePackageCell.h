11fe28901b78b8dac0acd4aefbb3a9896ac35ca3
Marius Rott <mariusrott@192-168-0-103.rdsnet.ro>
2013-09-11 11:33:05 +0300

    initial commit

---
+++ words/screens/cells/HomePackageCell.h
@@ -0,0 +1,15 @@
+//
+//  HomePackageCell.h
+//  words
+//
+//  Created by Marius Rott on 9/10/13.
+//  Copyright (c) 2013 mrott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomePackageCell : UITableViewCell
+
+@property (nonatomic, retain) IBOutlet UILabel *labelName;
+
+@end
---
+++ words/screens/cells/HomeQuestCell.h
@@ -0,0 +1,16 @@
+//
+//  HomeQuestCell.h
+//  words
+//
+//  Created by Marius Rott on 9/10/13.
+//  Copyright (c) 2013 mrott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeQuestCell : UITableViewCell
+
+@property (nonatomic, retain) IBOutlet UILabel *labelName;
+@property (nonatomic, retain) IBOutlet UIButton *buttonSkip;
+
+@end
