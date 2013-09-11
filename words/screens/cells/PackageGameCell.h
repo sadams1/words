dc8f373e9823c9f04ca90dee8b4f96e76084fb0a
Marius Rott <mariusrott@192-168-0-103.rdsnet.ro>
2013-09-11 12:44:25 +0300

    game cell initial commit

---
+++ words/screens/cells/PackageGameCell.h
@@ -0,0 +1,15 @@
+//
+//  PackageGameCell.h
+//  words
+//
+//  Created by Marius Rott on 9/11/13.
+//  Copyright (c) 2013 mrott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PackageGameCell : UITableViewCell
+
+@property (nonatomic, retain) IBOutlet UILabel *labelName;
+
+@end
---
+++ words/screens/cells/PackageGameCell.m
@@ -0,0 +1,29 @@
+//
+//  PackageGameCell.m
+//  words
+//
+//  Created by Marius Rott on 9/11/13.
+//  Copyright (c) 2013 mrott. All rights reserved.
+//
+
+#import "PackageGameCell.h"
+
+@implementation PackageGameCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
