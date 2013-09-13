e16da10f976d777bca5c3b5fafc2a93046f8db4d
Marius Rott <mariusrott@192-168-0-103.rdsnet.ro>
2013-09-13 09:25:25 +0300

    core data, import json files

--- words/AppDelegate.h
+++ words/AppDelegate.h
@@ -14,4 +14,10 @@
 
 @property (strong, nonatomic) UINavigationController *navigationController;
 
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory;
+
 @end
