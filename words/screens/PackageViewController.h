6b30743b341a6e777405cdcea9c76d79da79518d
Marius Rott <mariusrott@192-168-0-103.rdsnet.ro>
2013-09-13 09:18:50 +0300

    back & table

--- words/screens/PackageViewController.h
+++ words/screens/PackageViewController.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+@class Category;
+@class PackageGameCell;
+
 @interface PackageViewController : UIViewController
+
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
+
+@property (nonatomic, retain) IBOutlet PackageGameCell *cellGame;
+@property (nonatomic, retain) UINib *cellLoaderGame;
+
+- (id)initWithCategory:(Category*)category;
+
+- (IBAction)doButtonBack:(id)sender;
 
 @end
--- words/screens/PackageViewController.m
+++ words/screens/PackageViewController.m
@@ -7,34 +7,58 @@
 //
 
 #import "PackageViewController.h"
+#import "GameViewController.h"
+#import "CoreDataUtils.h"
+#import "Category.h"
+#import "Game.h"
+#import "PackageGameCell.h"
 
 @interface PackageViewController ()
+
+@property (nonatomic, retain) Category *category;
+@property (nonatomic, retain) NSArray *games;
+
+- (void)configureGameCell:(PackageGameCell *)cell atIndexPath:(NSIndexPath *)indexPath;
 
 @end
 
 @implementation PackageViewController
 
-- (id)init
+- (id)initWithCategory:(Category *)category
 {
     NSString *xib = @"PackageViewController";
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
     {
         xib = @"PackageViewController_iPad";
     }
-    self = [self initWithNibName:xib bundle:nil];
-    if (self) {
+    self = [super initWithNibName:xib bundle:nil];
+    if (self)
+    {
+        self.category = category;
         
+        NSSortDescriptor *sortDescriptorID = [[NSSortDescriptor alloc] initWithKey:@"identifier"
+                                                                         ascending:YES];
+        self.games = [[self.category.games allObjects] sortedArrayUsingDescriptors:[NSArray arrayWithObject:sortDescriptorID]];
+        
+        //  load cell
+        NSString *xib = @"PackageGameCell";
+        if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+        {
+            xib = @"PackageGameCell_iPad";
+        }
+        self.cellLoaderGame = [UINib nibWithNibName:xib bundle:nil];
     }
     return self;
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (void)dealloc
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+    [self.category release];
+    [self.games release];
+    [self.tableView release];
+    [self.cellGame release];
+    [self.cellLoaderGame release];
+    [super dealloc];
 }
 
 - (void)viewDidLoad
@@ -43,10 +67,71 @@
     // Do any additional setup after loading the view from its nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.tableView reloadData];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)doButtonBack:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+#pragma mark tableView
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.category.games.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    PackageGameCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"TABLE_VIEW_TYPE_GAME"];
+    if (cell == nil) {
+        [self.cellLoaderGame instantiateWithOwner:self options:nil];
+        cell = self.cellGame;
+        self.cellGame = nil;
+    }
+    
+    [self.cellLoaderGame instantiateWithOwner:self options:nil];
+    
+    [self configureGameCell:cell
+                atIndexPath:indexPath];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    Game *game = [[self.category.games allObjects] objectAtIndex:indexPath.row];
+    GameViewController *gameViewCont = [[GameViewController alloc] initWithGame:game parentViewController:self];
+    [self.navigationController pushViewController:gameViewCont animated:YES];
+}
+
+- (void)configureGameCell:(PackageGameCell *)cell atIndexPath:(NSIndexPath *)indexPath
+{
+    Game *game = [self.games objectAtIndex:indexPath.row];
+    cell.labelName.text = game.name;
+    
+    NSNumber *sum = [game.sessions valueForKeyPath:@"@sum.points"];
+    NSString *points = [NSString stringWithFormat:@"%d", sum.intValue];
+    
+    cell.labelName.text = [NSString stringWithFormat:@"%@ --- %@", points, cell.labelName.text];
+}
+
+#pragma mark -
+
 
 @end
